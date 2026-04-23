d8d9b84ea0d056c630fccb24934731109174e854
marketingonlinepedrohenrique-oss <marketingonline.pedrohenrique@gmail.com>
2026-04-22 22:53:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,32 @@
 const express = require("express");
+const app = express();
 const { exec } = require("child_process");
 
-const app = express();
-app.use(express.json());
-
-app.post("/download", (req, res) => {
-  const { url } = req.body;
-
-  exec(`yt-dlp ${url} -o video.mp4`, (error) => {
-    if (error) {
-      return res.status(500).send("Erro ao baixar vídeo");
-    }
-
-    res.send("Vídeo baixado com sucesso");
-  });
+app.get("/", (req,res)=>{
+ res.send("Vídeo viral backend rodando");
 });
 
-app.get("/", (req, res) => {
-  res.send("Backend viral video rodando");
+app.get("/download", (req,res)=>{
+
+ const url = req.query.url;
+
+ if(!url){
+   return res.send("Envie uma URL");
+ }
+
+ exec(`yt-dlp -f best -o video.mp4 ${url}`, (error, stdout, stderr)=>{
+
+   if(error){
+     console.log(error);
+     return res.send("Erro ao baixar vídeo");
+   }
+
+   res.download("video.mp4");
+
+ });
+
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
-});
-app.get("/test", (req, res) => {
-
-  const { exec } = require("child_process");
-
-  exec("yt-dlp --version", (error, stdout, stderr) => {
-
-    if (error) {
-      return res.send("yt-dlp não instalado");
-    }
-
-    res.send("yt-dlp versão: " + stdout);
-
-  });
-
+app.listen(3000, ()=>{
+ console.log("Servidor rodando");
 });
