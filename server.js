e84141c1ba8e1160a2f26773f14e6024ab98fc48
marketingonlinepedrohenrique-oss <marketingonline.pedrohenrique@gmail.com>
2026-04-22 23:00:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-const express = require("express");
-const app = express();
-const { exec } = require("child_process");
+const express = require("express")
+const { exec } = require("child_process")
+
+const app = express()
 
 app.get("/", (req,res)=>{
- res.send("Vídeo viral backend rodando");
-});
+ res.send("Vídeo viral backend rodando")
+})
 
-app.get("/download", (req,res)=>{
+app.get("/download",(req,res)=>{
 
- const url = req.query.url;
+ const url = req.query.url
 
  if(!url){
-   return res.send("Envie uma URL");
+  return res.send("Envie uma URL")
  }
 
  exec(`yt-dlp -f best -o video.mp4 ${url}`, (error, stdout, stderr)=>{
 
-   if(error){
-     console.log(error);
-     return res.send("Erro ao baixar vídeo");
-   }
+  if(error){
+   console.log(error)
+   return res.send("Erro ao baixar vídeo")
+  }
 
-   res.download("video.mp4");
+  res.download("video.mp4")
 
- });
+ })
 
-});
+})
 
 app.listen(3000, ()=>{
- console.log("Servidor rodando");
-});
+ console.log("Servidor rodando")
+})
